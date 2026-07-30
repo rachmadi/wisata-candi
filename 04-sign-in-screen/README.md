979fb9cb073bb9834fb44f8c6f5e2761f5b2842c
Muhammad Rachmadi <rachmadi@gmail.com>
2026-07-30 14:13:20 +0700

    Add Meeting 14 - Sign In Screen

---
+++ 04-sign-in-screen/README.md
@@ -0,0 +1,53 @@
+# 04 - Sign In Screen
+
+## Deskripsi
+
+Pada materi ini dikembangkan **Sign In Screen** sebagai contoh penerapan form,
+TextFormField, dan pengelolaan state sederhana pada Flutter.
+
+## Tujuan Pembelajaran
+
+- Membuat Sign In Screen.
+- Menggunakan Form dan TextFormField.
+- Menggunakan TextEditingController.
+- Menggunakan InputDecoration.
+- Mengatur tampilan dengan Padding dan SingleChildScrollView.
+- Mengubah StatelessWidget menjadi StatefulWidget.
+- Mengimplementasikan show/hide password menggunakan `obscureText`.
+
+## Materi
+
+- Form
+- TextFormField
+- TextEditingController
+- InputDecoration
+- OutlineInputBorder
+- ElevatedButton
+- TextButton
+- SingleChildScrollView
+- StatefulWidget
+- obscureText
+
+## Hasil Akhir
+
+Pada akhir materi ini telah tersedia:
+
+- Form Sign In.
+- Input nama pengguna.
+- Input kata sandi.
+- Tombol Sign In.
+- Tombol Sign Up.
+- Show / Hide Password.
+- Tampilan yang responsif terhadap keyboard.
+
+## Catatan
+
+Implementasi berikut **belum** dilakukan pada materi ini:
+
+- Proses Sign In.
+- Validasi pengguna.
+- Navigasi ke Sign Up Screen.
+- Penyimpanan data pengguna.
+- SharedPreferences.
+
+Fitur-fitur tersebut akan diimplementasikan pada pokok bahasan berikutnya.
---
+++ 04-sign-in-screen/lib/screens/profile_screen.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+import 'package:wisata_candi/widgets/profile_info_item.dart';
+
+class ProfileScreen extends StatefulWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  //TODO: 1. Deklarasikan variabel yang dibutuhkan
+  bool isSignedIn = false;
+  String fullName = '';
+  String userName = '';
+  int favoriteCandiCount = 0;
+
+  //TODO: 5. Implementasi fungsi signIn
+  void signIn() {
+    setState(() {
+      isSignedIn = !isSignedIn;
+    });
+  }
+
+  //TODO: 6. Implementasi fungsi signOut
+  void signOut() {
+    setState(() {
+      isSignedIn = !isSignedIn;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            height: 200,
+            width: double.infinity,
+            color: Colors.deepPurple,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Column(
+              children: [
+                //TODO: 2. Buat bagian ProfileHeader yang berisi gambar profil
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 200 - 50),
+                    child: Stack(
+                      alignment: Alignment.bottomRight,
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.deepPurple,
+                              width: 2,
+                            ),
+                            shape: BoxShape.circle,
+                          ),
+                          child: CircleAvatar(
+                            radius: 50,
+                            backgroundImage: AssetImage(
+                              'images/placeholder_image.png',
+                            ),
+                          ),
+                        ),
+                        if (isSignedIn)
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.camera_alt,
+                              color: Colors.deepPurple[50],
+                            ),
+                          ),
+                      ],
+                    ),
+                  ),
+                ),
+                //TODO: 3. Buat bagian ProfileInfo yang berisi info profil
+                SizedBox(height: 20),
+                Divider(color: Colors.deepPurple[100]),
+                SizedBox(height: 4),
+                ProfileInfoItem(
+                  icon: Icons.lock,
+                  label: 'Pengguna',
+                  value: userName,
+                  iconColor: Colors.amber,
+                ),
+                SizedBox(height: 4),
+                Divider(color: Colors.deepPurple[100]),
+                SizedBox(height: 4),
+                ProfileInfoItem(
+                  icon: Icons.person,
+                  label: 'Nama',
+                  value: fullName,
+                  showEditIcon: isSignedIn,
+                  onEditPressed: () {
+                    debugPrint('Icon edit ditekan ...');
+                  },
+                  iconColor: Colors.blue,
+                ),
+                SizedBox(height: 4),
+                Divider(color: Colors.deepPurple[100]),
+                SizedBox(height: 4),
+                ProfileInfoItem(
+                  icon: Icons.favorite,
+                  label: 'Favorit',
+                  value: favoriteCandiCount > 0 ? '$favoriteCandiCount' : '',
+                  iconColor: Colors.red,
+                ),
+                //TODO: 4. Buat bagian ProfileActions yang berisi TextButton sign in/out
+                SizedBox(height: 4),
+                Divider(color: Colors.deepPurple[100]),
+                SizedBox(height: 20),
+                isSignedIn
+                    ? TextButton(onPressed: signOut, child: Text('Sign Out'))
+                    : TextButton(onPressed: signIn, child: Text('Sign In')),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
